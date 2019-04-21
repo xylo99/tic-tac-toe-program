550effaaaa2ab7391974b24c68b9233b3f9ab339
xylo99 <thandy1@umbc.edu>
2019-04-21 16:31:55 -0400

    tic tac toe game using client-server technology over TCP

---
+++ tttc.py
@@ -0,0 +1,40 @@
+import socket
+import sys
+
+IP   = ''
+port = 13037
+s    = None
+client_start = 'FALSE'
+
+#connect if ther are no socket errors
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    if len(sys.argv) == 3:
+       IP = sys.argv[2]
+    else:
+       client_start = sys.argv[1]
+       IP = sys.argv[3]
+    s.connect((IP, port))
+    #we indicate whether or not the client starts to the server
+    #
+    s.send(client_start)
+    data = s.recv(1024)
+    print data
+except socket.error, (value, message):
+    if s:
+        s.close()
+    print "Could not open socket: " + message
+    sys.exit(1)
+#send and recieve gamedata until we recieve indication to end the connection
+while True:
+    data = s.recv(1024)
+    if data:
+       if data[:1] == 'e':
+          print data[1:]
+          break
+       else:
+          print data
+    data = raw_input('> ')
+    s.sendall(data)
+s.close()
